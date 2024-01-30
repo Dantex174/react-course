1b98a47d0982549bcb625b6e2071f6424451bfd7
Grigory Saifullin <grigory.saifullin@yandex.ru>
2024-01-30 18:38:07 +0300

    init

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,6 +7,13 @@ import EmployeesAddForm from "../employees-add-form/employees-add-form";
 import './app.css';
 
 function App() {
+
+    const data = [
+        {name: "John C.", salary: 800, increase: false, key: 1},
+        {name: "Alex M.", salary: 3000, increase: false, key: 2},
+        {name: "Carl W.", salary: 5000, increase: false, key: 3}
+    ];
+
     return (
         <div className='app'>
             <AppInfo/>
@@ -16,7 +23,9 @@ function App() {
                 <AppFilter/>
             </div>
 
-            <EmployeesList/>
+            <EmployeesList
+                data={data}
+                onDelete={id => console.log(id)}/>
             <EmployeesAddForm/>
         </div>
     )
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,24 +1,65 @@
 import './employees-list-item.css';
+import {Component} from "react";
 
-const EmployeesListItem = () => {
-    return (
-        <li className="list-group-item d-flex justify-content-between">
-            <span className="list-group-item-label">John Smith</span>
-            <input type="text" className="list-group-item-input" defaultValue="1000$"/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                        className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-                </button>
+class EmployeesListItem extends Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            increase: false,
+            favorite: false,
+        }
+    }
 
-                <button type="button"
-                        className="btn-trash btn-sm ">
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    )
+    onIncrease = () => {
+        this.setState(({increase}) => ({
+            increase: !increase
+        }))
+    }
+
+    onFavorite = () => {
+        this.setState(({favorite}) => ({
+            favorite: !favorite
+        }))
+    }
+
+    render() {
+
+        const {name, salary, onDelete} = this.props;
+        const {increase, favorite} = this.state;
+
+        let className = "list-group-item d-flex justify-content-between";
+        if(increase) {
+            className += ' increase';
+        }
+        if(favorite) {
+            className += ' like'
+        }
+        return (
+            <li className={className}>
+                <span className="list-group-item-label"
+                      onClick={this.onFavorite}>
+                      {name}
+                </span>
+                <input type="text"
+                       className="list-group-item-input"
+                       defaultValue={salary + '$'}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                            className="btn-cookie btn-sm"
+                            onClick={this.onIncrease}>
+                        <i className="fas fa-cookie"></i>
+                    </button>
+
+                    <button type="button"
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        )
+    }
 }
 
 export default EmployeesListItem;
